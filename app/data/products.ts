275ab5a06c8fba3df6c25b62436ea166fd77360c
Coraline-Fits <jhenykmoliveira@gmail.com>
2026-06-22 19:00:42 -0300

    criar catalogo

---
+++ app/data/products.ts
@@ -0,0 +1,73 @@
+export const products = [
+
+{
+id:"1",
+
+nome:"Vestido Coraline Azul",
+
+slug:"vestido-coraline-azul",
+
+preco:39.90,
+
+imagem:
+"https://placehold.co/700x900",
+
+categoria:
+"Vestidos",
+
+delivery:{
+tipo:"link",
+
+conteudo:
+"https://entrega.com"
+},
+
+postPurchase:{
+
+titulo:
+"Obrigada pela compra 💙",
+
+mensagem:
+"Seu item já está disponível."
+}
+
+},
+
+{
+id:"2",
+
+nome:"Conjunto Dark Doll",
+
+slug:
+"conjunto-dark-doll",
+
+preco:49.90,
+
+imagem:
+"https://placehold.co/700x900",
+
+categoria:
+"Conjuntos",
+
+delivery:{
+
+tipo:"texto",
+
+conteudo:
+"Entrega automática"
+
+},
+
+postPurchase:{
+
+titulo:
+"Compra aprovada",
+
+mensagem:
+"Acesse seu item."
+
+}
+
+}
+
+]
